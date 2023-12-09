78f19e56cb85a0c14c611208a7c536806040f2f5
maorni-huji <maor.nimri@huji.mail.ac.il>
2023-12-10 00:41:52 +0200

    Started connecting the server with google forms and added a server interaction with the program's runner

---
+++ Server/server_control.py
@@ -0,0 +1,44 @@
+from Server.tournament import Tournament
+
+
+def server_control():
+    """
+    Controls the server by the program's runner
+    :return: None
+    """
+    tournament = Tournament()
+    command = ""
+
+    print("Welcome to Server Control")
+
+    while command != "EXIT":
+        command = input("\n > Enter command: ")
+
+        if "?" == command:  # show menu
+            print("? - show list of commands",
+                  "EXIT - exit the server control",
+                  "FETCH_P - fetch participants from Google forms",
+                  "FETCH_W - fetch winning users from Google forms",
+                  "UPDATE_IN - update the groups that are still in the game for Google forms",
+                  "EDIT_GROUP - edit the details of a specific group",
+                  sep="\n")
+
+        elif "FETCH_P" == command or "FETCH_W" == command:
+            path = input(" > Enter the location of the file to load data from:")
+            while input(" > Fetch " + ("participants" if "FETCH_P" == command else "winners") + "? (Y/N) ") != "Y":
+                path = input(" > Enter the location of the file to load data from:")
+
+            tournament.fetch_participants(path) if "FETCH_P" == command else tournament.fetch_winners(path)
+
+            print(" < " + ("participants" if "FETCH_P" == command else "winners") + " fetched successfully")
+
+        elif "UPDATE_IN" == command:
+            tournament.update_google_form()
+
+        elif "EDIT_GROUP" == command:
+            pass
+
+        elif "EXIT" != command:
+            print(" < Unknown command")
+
+    print(" > Bye bye")
