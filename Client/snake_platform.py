dd6efa71dc1a9dada5387e0b800dc749b2165c7d
Noam Weingarten <noamwein2005@gmail.com>
2023-12-09 15:10:20 +0200

    Add useful libraries

--- Client/snake_platform.py
+++ Client/snake_platform.py
@@ -1,1 +1,118 @@
-#Just checking to see that it's working.
+#The actual snake game that utilises the brains provided by the two players.
+import pygame
+import sys
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 600, 400
+GRID_SIZE = 20
+SNAKE_SIZE = 20
+FPS = 10
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Snake class
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((WIDTH // 2), (HEIGHT // 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = GREEN
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def update(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * GRID_SIZE)) % WIDTH), (cur[1] + (y * GRID_SIZE)) % HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+        self.length = 1
+        self.positions = [((WIDTH // 2), (HEIGHT // 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def render(self, surface):
+        for p in self.positions:
+            pygame.draw.rect(surface, self.color, (p[0], p[1], SNAKE_SIZE, SNAKE_SIZE))
+
+
+# Food class
+class Food:
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = RED
+        self.randomize_position()
+    
+    def set_food_color(self, color):
+        self.color = color
+
+    def randomize_position(self):
+        self.position = (random.randint(0, (WIDTH // GRID_SIZE) - 1) * GRID_SIZE,
+                         random.randint(0, (HEIGHT // GRID_SIZE) - 1) * GRID_SIZE)
+
+    def render(self, surface):
+        pygame.draw.rect(surface, self.color, (self.position[0], self.position[1], SNAKE_SIZE, SNAKE_SIZE))
+
+
+# Direction vectors
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Main function
+def main():
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+
+    snake = Snake()
+    food = Food()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.W:
+                    if snake.direction != DOWN:
+                        snake.direction = UP
+                elif event.key == pygame.S:
+                    if snake.direction != UP:
+                        snake.direction = DOWN
+                elif event.key == pygame.A:
+                    if snake.direction != RIGHT:
+                        snake.direction = LEFT
+                elif event.key == pygame.D:
+                    if snake.direction != LEFT:
+                        snake.direction = RIGHT
+
+        snake.update()
+        if snake.get_head_position() == food.position:
+            snake.length += 1
+            food.randomize_position()
+        surface.fill(BLACK)
+        snake.render(surface)
+        food.render(surface)
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
+
+if __name__ == "__main__":
+    main()
