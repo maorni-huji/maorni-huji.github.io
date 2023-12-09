d192fc3131d1f1057ffd6a781d1ac519512d7a5a
maorni-huji <maor.nimri@huji.mail.ac.il>
2023-12-09 11:13:16 +0200

    Added demo-snake file for the users to play with while they are making their own snake

---
+++ Client/default_snake.py
@@ -0,0 +1,2 @@
+# The default snake the program uses so the users can play with their snakes and check them
+# This file's structure is similar to your_snake.py's structure, but it also includes the way the demo-snake should move
